2284523da4a66de7fd63041862fe503ec78b767d
Faishalzufar23 <82081205+Faishalzufar23@users.noreply.github.com>
2023-06-28 22:06:43 +0700

    Add files via upload

---
+++ storage/framework/views/88a1800f7ea7d1818fa725c7bba5156d.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app1', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\faish\Downloads\Controller_view\resources\views/home.blade.php ENDPATH**/ ?>
